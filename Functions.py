ec12eadd151f1dbc20a6a1e1d7830bb4730c8f28
Corentin <corentin.dieval@gmail.com>
2019-01-29 18:40:17 +0100

    Test d'affichage de graphe, a perfectionner...

--- Functions.py
+++ Functions.py
@@ -3,81 +3,44 @@
 
 #Imports
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import math
 
 
 # Fonction Serie Inverses Carrés
-
+# 1.
 def SerieInvCarrees(N):
     S = 0
-    for k in rnage (1, N+1):
+    for k in range(1, N+1):
         S += 1/(k**2)
     return S
 
+# 2.
 def MethodeSerieInvCarres(N):
     s = SerieInvCarrees(N)
     return math.sqrt(6*s)
 
+# Fonction Serie Inverses Carrés Impairs
+# 3.
 def SerieInvCarresImpairs(N):
     S = 0
-    for k in range (0, N+1):
-        S += 1/(2*k+1)**2
+    for k in range(0, N+1):
+        S += 1/((2*k+1)**2)
     return S
 
+# 4.
 def MethodeSerieInvCarresImpairs(N):
     s = SerieInvCarresImpairs(N)
     return math.sqrt(8*s)
 
+# 5.
+# Représentation de deux graphes : |MethodeSerieXXX(N) - PI|
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-# make up some data in the interval ]0, 1[
-y = np.random.normal(loc=0.5, scale=0.4, size=1000)
-y = y[(y > 0) & (y < 1)]
-y.sort()
-x = np.arange(len(y))
-
-
-
-# linear
-plt.subplot(221)
-plt.plot(x, y)
-plt.yscale('linear')
-plt.title('linear')
-plt.grid(True)
-
-
-# log
-plt.subplot(222)
-plt.plot(x, y)
-plt.yscale('log')
-plt.title('log')
-plt.grid(True)
-
-
-# symmetric log
-plt.subplot(223)
-plt.plot(x, y - y.mean())
-plt.yscale('symlog', linthreshy=0.01)
-plt.title('symlog')
-plt.grid(True)
-
-# logit
-plt.subplot(224)
-plt.plot(x, y)
-plt.yscale('logit')
-plt.title('logit')
-plt.grid(True)
-# Format the minor tick labels of the y-axis into empty strings with
-# `NullFormatter`, to avoid cumbering the axis with too many labels.
-plt.gca().yaxis.set_minor_formatter(NullFormatter())
-# Adjust the subplot layout, because the logit one may take more space
-# than usual, due to y-tick labels like "1 - 10^{-3}"
-plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
-                    wspace=0.35)
-
-plt.show()
-
+def GraphMethodeXXX(min, max):
+    x = []
+    for k in range(min, max+1):
+        x.append(k)
+    y = MethodeSerieInvCarres(x)
+    plt.plot(x, y)
+    plt.show()
 
