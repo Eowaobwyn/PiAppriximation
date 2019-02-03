821ba1c925d2eb1795c1b67f226b769c10f1c364
Thibaud REY <contact@thibaudrey.pro>
2019-02-03 22:16:23 +0100

    Monte carlo

--- Functions.py
+++ Functions.py
@@ -2,13 +2,17 @@
 ### Pi approximation
 
 # Imports
+from random import random
+
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-
 # Fonction Serie Inverses Carres
 # 1.
+from numpy.matlib import rand
+
+
 def SerieInvCarrees(N):
     S = 0
     for k in range(1, N + 1):
@@ -41,7 +45,7 @@ def MethodeSerieInvCarresImpairs(N):
 # Representation de deux graphes : |MethodeSerieXXX(N) - PI|
 
 def graph_method_xxx(min, max):
-    x = np.arange(min, max+1, 1)
+    x = np.arange(min, max + 1, 1)
     y1 = []
     y2 = []
     for k in range(min, max + 1):
@@ -50,3 +54,31 @@ def graph_method_xxx(min, max):
 
     plt.plot(x, y1, y2)
     plt.show()
+
+
+def method_serie_ramanujan(n):
+    S = 0
+    for k in range(0, n + 1):
+        S += (math.factorial(4 * k) / math.factorial(k) ** 4) * (1103 + (26390 * k)) / (4 * 99) ** (4 * k)
+    return ((2 * math.sqrt(2) / 9801) * S) ** (-1)
+
+
+def monte_carlo(n):
+    fig, ax = plt.subplots()
+    k = 0
+
+    for i in range(1, n + 1):
+        x, y = random(), random()
+        if x ** 2 + y ** 2 > 1:
+            color = 'red'
+        else:
+            color = 'green'
+            k += 1
+
+        ax.scatter(x, y, c=color, s=10, label=color, alpha=1, edgecolors='none')
+
+    # ax.legend()
+    ax.grid(True)
+
+    plt.show()
+    return k/n
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from Functions import SerieInvCarrees, graph_method_xxx
+from Functions import SerieInvCarrees, graph_method_xxx, method_serie_ramanujan, monte_carlo
 
-graph_method_xxx(1, 500)
+# graph_method_xxx(1, 500)
+# print(method_serie_ramanujan(10000))
+print(monte_carlo(5000))
